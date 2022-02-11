2949e041ecf7eecca6dfa13967e7ed7e4e7c96e7
yuomatsu <96334534+yuomatsu@users.noreply.github.com>
2022-02-11 21:24:22 +0900

    66. globalなスタイル

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import "./styles.css";
+import { Button, ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Button colorScheme="teal">ボタン</Button>
+      <p>てすと</p>
+    </ChakraProvider>
   );
 }
