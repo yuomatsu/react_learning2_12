2af25f39871ead53ea6bba229f3e7c988345ba20
yuomatsu <96334534+yuomatsu@users.noreply.github.com>
2022-02-12 13:47:56 +0900

    67. ルーティング

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Button, ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
 import theme from "./theme/theme";
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal">ボタン</Button>
-      <p>てすと</p>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
