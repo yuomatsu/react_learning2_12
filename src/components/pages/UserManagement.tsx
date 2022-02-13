abe1010ec9b05802a98f415a99c8208edc41aefc
yuomatsu <96334534+yuomatsu@users.noreply.github.com>
2022-02-13 19:50:22 +0900

    75. ユーザー一覧画面作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,18 @@
 import { memo, VFC } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+
+import { UserCard } from "../organisms/user/userCard";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>UserManegementページです。</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="おまつ"
+          fullName="yu"
+        ></UserCard>
+      </WrapItem>
+    </Wrap>
+  );
 });
