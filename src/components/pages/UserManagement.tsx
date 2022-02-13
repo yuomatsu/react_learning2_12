ed8e0a0d344952ffd8bd0f8d7aad67a0262035a9
yuomatsu <96334534+yuomatsu@users.noreply.github.com>
2022-02-13 22:33:05 +0900

    76. ユーザー一覧取得機能

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,18 +1,36 @@
-import { memo, VFC } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { memo, useEffect, VFC } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
 
 import { UserCard } from "../organisms/user/userCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  // 画面表示初回だけ関数実行
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="おまつ"
-          fullName="yu"
-        ></UserCard>
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="center">
+          {users.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
